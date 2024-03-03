b3611dc9ee5c26fef202f23eb96f814354d9f46d
rokoperisic <roko.perisic@gmail.com>
2024-03-04 00:11:36 +0100

    Filter books feature

--- src/DisplayBooks.jsx
+++ src/DisplayBooks.jsx
@@ -1,5 +1,16 @@
+import { genres } from "./books";
+import { useState } from "react";
+
+const createGenreOptions = () => {
+    return Object.keys(genres).map(genre => (
+      <option key={genre} value={genre}>
+        {genres[genre]}
+      </option>
+    ));
+};
+
 const createBookCard = (books) => {
-    const sortedBooks = books.books.sort((a, b) => {
+    const sortedBooks = books.sort((a, b) => {
         if (a.author !== b.author) {
             return a.author.localeCompare(b.author);
         }
@@ -26,25 +37,54 @@ const createBookCard = (books) => {
                 <p>Izdavačka kuća: {book.publishingHouse}</p>
                 <p>Godina izdavanja: {book.yearOfPublication}</p>
                 <p>Žanr: {book.genres}</p>
-                <p>Dostupne kopije: {book.avaiableCopies}</p>
+                <p>Broj dostupnih knjiga: {book.avaiableCopies}</p>
             </div>
         </div>
     ));
 }
 
-function DisplayBooks (books ) {
+function DisplayBooks(books ) {
+    const [filterName, setFilterName] = useState('');
+    const [filterGenre, setFilterGenre] = useState('');
 
-  return (
-    <div>
-        <h2>Knjige</h2>
-        <div className="display-books-container">
-            <div>aa</div>
-            <div className='books-container'>
-                {createBookCard(books)}
+    const filteredBooks = books.books.filter(book => {
+        const nameMatch = book.title.toLowerCase().startsWith(filterName.toLowerCase());
+        const genreMatch = filterGenre === '' || book.genres === genres[filterGenre];
+        return nameMatch && genreMatch;
+    });
+
+    const handleNameChange = (event) => {
+        setFilterName(event.target.value);
+    };
+
+    const handleGenreChange = (event) => {
+        setFilterGenre(event.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Knjige</h2>
+            <div className="display-books-container">
+                <div className="filter-books-container">
+                    <h2>Pretraži knjigu</h2>
+                    <div className="filter-books-name-container">
+                        <h3>Ime knjige</h3>
+                        <input type="text" className="filter-books-name-input" value={filterName} onChange={handleNameChange} />
+                    </div>
+                    <div className="filter-books-genre-container">
+                        <h3>Žanr</h3>
+                        <select className='filter-genre' value={filterGenre} onChange={handleGenreChange}>
+                            <option value="">Svi žanrovi</option>
+                            {createGenreOptions()}
+                        </select>
+                    </div>
+                </div>
+                <div className='books-container'>
+                    {createBookCard(filteredBooks)}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
 export default DisplayBooks;
